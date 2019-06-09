a75e4620b44224e79ef8f1fc080363d139a50aaa
mjgomsa <46731471+mjgomsa@users.noreply.github.com>
2019-06-09 13:25:15 -0600

    changes in php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,22 +1,67 @@
 <?php
-    $name = $_POST['name'];
-    $visitor_email =$_POST['email'];
-    $message = $_POST['message'];
 
-    $email_from = $_POST['email']; // required
-//    $email_from = 'info@genera.cr';
-    
-    $email_subject = 'Nuevo Mensaje de Página Genera';
-    
-    $email_body = "Nombre del Usuario: $name. \n".
-                    "E-mail del Usuario: $visitor_email. \n".
-                        "Mensaje del Usuario: $message. \n";
-    
-    $to = "asaavedra@gasehi.com";
-    $headers = "From: $email_from \r\n";
-    $headers = "Reply-To: $visitor_email \r\n";
+$errorMSG = "";
 
-    mail($to, $email_subject, $email_body, $headers);
+// NAME
+if (empty($_POST["name"])) {
+    $errorMSG = "Name is required ";
+} else {
+    $name = $_POST["name"];
+}
 
-    header("Location: index.html");
+// EMAIL
+if (empty($_POST["email"])) {
+    $errorMSG .= "Email is required ";
+} else {
+    $email = $_POST["email"];
+}
+
+// MSG SUBJECT
+if (empty($_POST["msg_subject"])) {
+    $errorMSG .= "Subject is required ";
+} else {
+    $msg_subject = $_POST["msg_subject"];
+}
+
+
+// MESSAGE
+if (empty($_POST["message"])) {
+    $errorMSG .= "Message is required ";
+} else {
+    $message = $_POST["message"];
+}
+
+//Add your email here
+$EmailTo = "majogosaa98@gmail.com";
+$Subject = "New Message Received";
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+$Body .= "Subject: ";
+$Body .= $msg_subject;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $message;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+
+// redirect to success page
+if ($success && $errorMSG == ""){
+   echo "success";
+}else{
+    if($errorMSG == ""){
+        echo "Something went wrong :(";
+    } else {
+        echo $errorMSG;
+    }
+}
+
 ?>
